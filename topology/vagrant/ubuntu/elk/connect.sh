83c44ec35cc5f7766abf9fa53914b6733e418f5a
ClaudioLista <cl.lista@studenti.unina.it>
2023-12-02 14:39:41 +0100

    Generalizzazione regole redirection controller

--- topology/vagrant/ubuntu/elk/connect.sh
+++ topology/vagrant/ubuntu/elk/connect.sh
@@ -3,20 +3,20 @@ echo $1
 echo "REDIRECT TO COWRIE SSH"
 curl -X POST \
    -H 'Content-Type: application/json' \
-   -d "{\"Source_IP\": \"$1\", \"Dpid\": \"85884017520972\"}" \
-   http://192.168.5.100:8080/rest_controller/redirect_ssh_int
+   -d "{\"Source_IP\": \"$1\", \"Dpid\": \"85884017520972\", \"Tcp_port\": \"22\", \"Decoy\": \"heralding1\", \"Source\": \"service\"}" \
+   http://192.168.5.100:8080/rest_controller/redirect_traffic
 
-echo "REDIRECT TO HERALDING"
+echo "REDIRECT TO HERALDING FTP"
 curl -X POST \
    -H 'Content-Type: application/json' \
-   -d "{\"Source_IP\": \"$1\", \"Dpid\": \"85884017520972\"}" \
-   http://192.168.5.100:8080/rest_controller/redirect_to_heralding
+   -d "{\"Source_IP\": \"$1\", \"Dpid\": \"85884017520972\", \"Tcp_port\": \"21\", \"Decoy\": \"heralding1\", \"Source\": \"service\"}"\
+   http://192.168.5.100:8080/rest_controller/redirect_traffic
 
 echo "REDIRECT TO COWRIE SMTP TELNET"
 curl -X POST \
    -H 'Content-Type: application/json' \
-   -d "{\"Source_IP\": \"$1\", \"Dpid\": \"85884017520972\"}" \
-   http://192.168.5.100:8080/rest_controller/redirect_to_cowrie_telnet
+   -d "{\"Source_IP\": \"$1\", \"Dpid\": \"85884017520972\", \"Tcp_port\": \"23\", \"Decoy\": \"cowrie\", \"Source\": \"service\"}"\
+   http://192.168.5.100:8080/rest_controller/redirect_traffic
 
 echo "REDIRECT TO COWRIE HTTP PORT HOPPING"
 curl -X POST \
