a25113467185e38c8ee20e24122a35e6bed1e3f9
ClaudioLista <cl.lista@studenti.unina.it>
2024-01-26 13:30:57 +0100

    aggiunta in script generazione dinamica LIH

--- topology/lan1/docker/docker-build/configurations/controller/rest_controller/topology.py
+++ topology/lan1/docker/docker-build/configurations/controller/rest_controller/topology.py
@@ -1,12 +1,14 @@
 from network import Host, Honeypot, Attacker, Subnet, Network, Gateway
 
-#------- NETWORK TOPOLPOGY -------------------------------------------------------------- #    
+#------- NETWORK TOPOLPOGY LAN1 -------------------------------------------------------------- #    
 # Nodes
+#Subnet 1
 host = Host('host', '10.1.3.10', '08:00:27:b6:d0:66', 15, '255.255.255.0')
 service = Host('service', '10.1.3.11', '08:00:27:6d:ec:62', 3, '255.255.255.0')
 ssh_service = Host('ssh_service', '10.1.3.13', '08:00:27:b6:d0:69', 16, '255.255.255.0')
 heralding = Honeypot('heralding', '10.1.3.12', '08:00:27:6c:0a:bf', 4, '255.255.255.0')
 
+#Subnet 2
 cowrie = Honeypot('cowrie', '10.1.4.10', '08:00:27:b7:0e:58', 6, '255.255.255.0')
 heralding1 = Honeypot('heralding1', '10.1.4.11', '08:00:27:6d:f9:98', 13, '255.255.255.0')
 
@@ -18,6 +20,7 @@ dmz_service = Host('dmz_service', '10.1.10.11', '08:00:27:b6:d0:67', 22, '255.25
 dmz_service1 = Host('dmz_service1', '10.1.10.14', '08:00:27:6d:ec:74', 21, '255.255.255.0')
 dmz_cowrie = Honeypot('dmz_cowrie', '10.1.10.13', '08:00:27:b7:0e:59', 20, '255.255.255.0')
 dmz_host = Host('dmz_host', '10.1.10.12', '08:00:27:b6:d0:68', 23, '255.255.255.0')
+
 
 # Subnets
 # ovs1
@@ -34,8 +37,8 @@ gw1 = Gateway('gw1', '10.1.3.1', '9e:c3:c6:49:0e:e8', 1, '255.255.255.0')
 gw2 = Gateway('gw2', '10.1.4.1', '16:67:1f:3f:86:a7', 5, '255.255.255.0')
 gw3 = Gateway('gw3', '10.1.5.1', 'fe:46:67:35:0d:d1', 7, '255.255.255.0')
 # ovs2
-gw10 = Gateway('gw10', '10.1.10.1', '8a:ae:02:40:8f:83', 40, '255.255.255.0')
-gw11 = Gateway('gw11', '10.1.11.1', 'ea:6a:20:a0:96:10', 41, '255.255.255.0')
+gw10 = Gateway('gw10', '10.1.10.1', '8a:ae:02:40:8f:83', 10, '255.255.255.0')
+gw11 = Gateway('gw11', '10.1.11.1', 'ea:6a:20:a0:96:10', 11, '255.255.255.0')
 
 # Network
 network1 = Network('Net1')
--- topology/lan1/vagrant/ubuntu/utils.sh
+++ topology/lan1/vagrant/ubuntu/utils.sh
@@ -42,3 +42,48 @@ create_tap() {
     sudo ovs-vsctl add-port $bridge_name $tap_name tag=$tag -- set interface $tap_name ofport=$ofport
   fi
 }
+
+find_free_ip() {
+  local subnet=$1
+
+  # Estrai l'indirizzo della rete e il prefisso dalla sottorete
+  network=$(echo "$subnet" | cut -d '/' -f 1)
+  prefix=$(echo "$subnet" | cut -d '/' -f 2)
+
+  # Calcola il numero massimo di host possibili nella sottorete
+  max_hosts=$((2**(32 - prefix) - 2))
+
+  # Trova gli IP attualmente in uso nella sottorete
+  used_ips=$(arp-scan --localnet | grep -oP '\(\K[^)]+')
+
+  # Loop per trovare il primo IP libero
+  for i in $(seq 1 $max_hosts); do
+    test_ip="$network.$i"
+
+    # Controlla se l'IP è libero
+    if ! echo "$used_ips" | grep -w "$test_ip" > /dev/null; then
+      echo "$test_ip"
+      return
+    fi
+  done
+
+  echo "Nessun indirizzo IP libero trovato nella sottorete $subnet"
+}
+find_free_ip2() {
+  subnet="$1"
+  
+  # Loop attraverso gli indirizzi IP nella subnet
+  for i in {2..254}; do
+    ip_address="${subnet%.*}.$i"
+    
+    # Utilizza ping per verificare se l'indirizzo IP è in uso
+    if ! ping -c 1 -w 1 "$ip_address" &> /dev/null; then
+      echo "$ip_address"
+      return
+    fi
+  done
+
+  echo "Nessun indirizzo IP libero trovato nella subnet $subnet"
+  exit 1
+}
+
