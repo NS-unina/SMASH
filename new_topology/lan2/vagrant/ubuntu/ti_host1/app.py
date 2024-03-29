9121f758c8eabefc43b4f39700d4f34b2437531e
ClaudioLista <cl.lista@studenti.unina.it>
2024-03-29 11:20:48 +0100

    ripristino docker-compose origianle dopo stop servere + aggiunta gestione caso in cui honeypot richiesto sia già presente

--- new_topology/lan2/vagrant/ubuntu/ti_host1/app.py
+++ new_topology/lan2/vagrant/ubuntu/ti_host1/app.py
@@ -1,11 +1,71 @@
 from flask import Flask, request
 import os
 import subprocess
+import signal
+import sys
+import os
 
 app = Flask(__name__)
 
+# Ottieni la directory corrente
+current_directory = os.getcwd()
+
+# Costruisci il percorso del file basato sulla directory corrente
+file_name = "docker-compose.yaml"
+file_path = os.path.join(current_directory, 'docker', 'docker-build',file_name)
+
+# Leggi il contenuto iniziale del file YAML
+with open(file_path, 'r') as f:
+    initial_config = f.read()
+
+# Definisci la funzione da eseguire al momento dell'arresto del server
+def cleanup():
+    # Scrivi il contenuto iniziale nel file YAML per ripristinarlo
+    with open(file_path, 'w') as f:
+        f.write(initial_config)
+
+# Collega la funzione di cleanup al segnale di interruzione (CTRL+C)
+def signal_handler(sig, frame):
+    cleanup()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+class Container:
+    def __init__(self, name = None, ssh_port = None, ftp_port = None, socks_port = None):
+        self.name = name
+        self.ssh_port = ssh_port
+        self.ftp_port = ftp_port
+        self.socks_port = socks_port
+        
+    # GET METHODS
+    def get_name(self):
+        return self.name
+    
+    def get_ssh_port(self):
+        return self.ssh_port
+    def get_ftp_port(self):
+        return self.ftp_port
+    def get_socks_port(self):
+        return self.socks_port
+
+    # SET METHODS
+    def set_name(self, name):
+        self.name = name   
+    def set_ssh_port(self, ssh_port):
+        self.ssh_port = ssh_port
+    def set_ftp_port(self, ftp_port):
+        self.ftp_portp_port = ftp_port
+    def set_socks_port(self, socks_port):
+        self.socks_port = socks_port
+
+
 # 172.28.0.2 172.28.0.3
 ip_address_container = [2,3]
+containers_name = []
+containers = []
+
 
 @app.route('/handle_post', methods=['POST'])
 def handle_post():
@@ -19,15 +79,23 @@ def handle_post():
     socks_port = request.form.get('socks_port')
     # Ottieni il percorso assoluto della cartella corrente
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    
+    new_container = Container(name,ssh_port,ftp_port,socks_port)
     # Costruisci il percorso completo dello script
     script_path = os.path.join(current_directory, 'docker', 'script.sh')
     
-    # Lista di parametri da passare allo script
-    parameters = [name, ip, ssh_port, ftp_port,socks_port]
-    # Esegui lo script Bash quando viene ricevuta una richiesta POST
-    subprocess.call([script_path] + parameters)
-    return "Script eseguito con successo", 200
+    if name not in containers_name:
+
+        # Lista di parametri da passare allo script
+        parameters = [name, ip, ssh_port, ftp_port,socks_port]
+        # Esegui lo script Bash quando viene ricevuta una richiesta POST
+        subprocess.call([script_path] + parameters)
+        containers_name.append(name)
+        containers.append(new_container)
+
+        return "Script eseguito con successo", 200
+    else:
+        print("CONTAINER GIA PRESENTE")
+        return "Script eseguito con successo", 200
 
 def find_free_address(address_list, start_address):
     # Itera attraverso le porte nella lista a partire dalla porta specificata
